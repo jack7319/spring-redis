8a356bfbb0508e4635e2d8e5e5b80c8484a7efeb
leslie2180 <2112407533@qq.com>
2017-07-25 14:18:16 +0800

    增加swagger2配置

--- src/main/java/com/bizideal/mn/controller/CityController.java
+++ src/main/java/com/bizideal/mn/controller/CityController.java
@@ -3,15 +3,21 @@ package com.bizideal.mn.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bizideal.mn.bean.City;
 import com.bizideal.mn.service.CityService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * @author 作者 liulq:
@@ -19,6 +25,7 @@ import com.bizideal.mn.service.CityService;
  * @version 1.0
  * @description 描述
  */
+@Api(value = "测试接口")
 @Controller
 @RequestMapping("/city")
 public class CityController {
@@ -26,13 +33,15 @@ public class CityController {
 	@Autowired
 	private CityService cityService;
 
-	@RequestMapping("/get/{id}")
+	@ApiOperation(value = "根据id查询城市", httpMethod = "GET", notes = "根据id查询城市")
+	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
 	@ResponseBody
 	public City getById(@PathVariable("id") Integer id) {
 		return cityService.getById(id);
 	}
 
-	@RequestMapping("/update")
+	@ApiOperation(value = "根据id更新城市", httpMethod = "POST", notes = "根据id更新城市")
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, String> getById(@RequestBody City city) {
 		Map<String, String> map = new HashMap<String, String>();
